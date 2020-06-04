b448949b7a563384ce980b9d2f66f13a8fb4cb03
מתניה ילינק <matanya.je@hotmail.com>
2020-06-04 17:43:36 +0300

    centers HTMLs create html pages for every center and add to center info page in init the html that correspond to the id of the center

--- src/app/center-info/center-info.component.ts
+++ src/app/center-info/center-info.component.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 
-
 @Component({
   selector: 'app-center-info',
   templateUrl: './center-info.component.html',
   styleUrls: ['./center-info.component.css']
 })
 export class CenterInfoComponent implements OnInit {
-
-  public centerId="";
+  public centerId = "";
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe((params:ParamMap)=>{
-      let id=params.get('id');
-      this.centerId=id;
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let id = params.get('id');
+      this.centerId = id;
     });
+    document.getElementById("center-info").innerHTML = '<iframe width="100%"  onload=javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));  frameBorder="0" src="assets/centers/' + this.centerId + '.html"></iframe>';
   }
 
 }
+
+
